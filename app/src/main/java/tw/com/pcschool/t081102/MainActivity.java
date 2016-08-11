f211b3c4001741aab6e60afe570edcf1d2c10bb5
shaojung <shaojung@gmail.com>
2016-08-11 15:38:28 +0800

    Finish Notification.

--- app/src/main/java/tw/com/pcschool/t081102/MainActivity.java
+++ app/src/main/java/tw/com/pcschool/t081102/MainActivity.java
@@ -2,7 +2,9 @@ package tw.com.pcschool.t081102;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,10 +26,20 @@ public class MainActivity extends AppCompatActivity {
     public void click1(View v)
     {
         Notification.Builder builder = new Notification.Builder(context);
+        Intent intent = new Intent(context, MainActivity.class);
+        // 設定Intent 標誌參數
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
+        int requestCode = 1;
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Test")
-                .setContentText("123");
+                .setContentText("123")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
         Notification notification = builder.build();
         notificationManager.notify(123, notification); // 發佈Notification
     }
